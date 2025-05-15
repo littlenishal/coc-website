84b2cc21b883f076595f791a914f2ad7806c7449
Nishal <63165790+littlenishal@users.noreply.github.com>
2025-05-15 03:18:58 +0000

    swapped params

--- src/app/api/events/[id]/route.ts
+++ src/app/api/events/[id]/route.ts
@@ -2,8 +2,8 @@ import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 
 export async function GET(
-  _: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: { id: string } },
+  _: NextRequest
 ) {
   try {
     //const eventId = params.id;
